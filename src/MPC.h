c51760ba3ade8000b1f2cfe5f9dd2cb11166e1f3
johannesfrimberger <johannes.frimberger@gmail.com>
2017-05-29 22:00:24 +0200

    Re-Indent files and make IPOPT options constant

--- src/MPC.h
+++ src/MPC.h
@@ -7,14 +7,21 @@
 using namespace std;
 
 class MPC {
- public:
-  MPC();
-
-  virtual ~MPC();
-
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+public:
+    MPC();
+    
+    virtual ~MPC();
+    
+    // Solve the model given an initial state and polynomial coefficients.
+    // Return the first actuatotions.
+    vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+    
+private:
+    
+    std::string getOptionsString() const;
+    
+    // options for IPOPT solver
+    const std::string _options;
 };
 
 #endif /* MPC_H */
